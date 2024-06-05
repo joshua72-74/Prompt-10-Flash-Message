3d6c3a4f71364614e07e0bb8d6cab43d641fd159
joshuajoy <joshuajoy1108@gmail.com>
2024-06-05 18:31:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+val = Math.round(Math.random() * 100);
+val = Math.floor(Math.random() * 100);
+console.log(val);
+
+
+
+
+val = 0;
+count = 0;
+while(val!=69){
+    val = Math.floor(Math.random() * 100);
+    count++;
+    console.log(val, count);
+}
